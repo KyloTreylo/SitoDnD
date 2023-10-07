c1839b55e877c5ca7a271a1f23ec645a75e11390
Ghesh05 <samuel24ibrahimi@gmail.com>
2023-10-07 11:24:05 +0200

    11:23
    ho aggiunto il css,js e html di personaggi. ho sistemato alcune cose in home lore html e css

---
+++ public/js/personaggi.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadMain()
+});
+
+async function loadMain() {
+
+    // Carica la tabella dei cer
+    const main = document.querySelector("main")
+    const title = document.getElementById("div-titolo")
+
+    title.innerHTML = `<h1>Edean</h1>`;
+
+	fetch('http://localhost:5000/html/personaggi.html')
+    .then(response => response.text())
+    .then((html) => {
+        main.innerHTML = html
+    })
+}

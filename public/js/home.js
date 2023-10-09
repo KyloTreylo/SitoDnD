50994bd2a3f56b2cb06f59124dde436cad6613a9
KyloTreylo <enneemme1975@gmail.com>
2023-10-09 17:42:50 +0200

    .

--- public/js/home.js
+++ public/js/home.js
@@ -30,7 +30,52 @@ async function loadMain() {
     .then(response => response.text())
     .then((data) => {
         main.innerHTML = data
+    }).then (e => {
+        const slides = document.querySelector('.slides');
+        const prevButton = document.querySelector('.prev');
+        const nextButton = document.querySelector('.next');
+        let currentIndex = 0;
+        const slideCount = document.querySelectorAll('.slide').length;
+        let timer;
+
+        function goToSlide(index) {
+            currentIndex = index;
+            const translateX = -currentIndex * 100;
+            slides.style.transform = `translateX(${translateX}%)`;
+        }
+
+        function nextSlide() {
+            currentIndex = (currentIndex + 1) % slideCount;
+            goToSlide(currentIndex);
+            clearInterval(timer);
+            startTimer();
+        }
+
+        function prevSlide() {
+            currentIndex = (currentIndex - 1 + slideCount) % slideCount;
+            goToSlide(currentIndex);
+            clearInterval(timer);
+            startTimer();
+        }
+
+        nextButton.addEventListener('click', () => {
+            nextSlide();
+        });
+
+        prevButton.addEventListener('click', () => {
+            prevSlide();
+        });
+
+        function startTimer() {
+            timer = setInterval(() => {
+                nextSlide();
+            }, 3000);
+        }
+
+        startTimer();
+ 
     })
+
 }
 
 

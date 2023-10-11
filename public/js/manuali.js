d9d3e634cc79d4c71505927301414fda0610879d
KyloTreylo <enneemme1975@gmail.com>
2023-10-11 09:08:12 +0200

    Sistemazione sottocartelle immagini

--- public/js/manuali.js
+++ public/js/manuali.js
@@ -3,27 +3,34 @@ document.addEventListener('DOMContentLoaded', function () {
     let titolo = document.title;
     let nomeManuale;
     let manualePresente = true;
+    let nomejson;
 
     if (titolo=="Manuale del giocatore") {
         nomeManuale = "manuale-giocatore"
+        nomejson = "Manuale_del_Giocatore"
     } else if (titolo=="Manuale dei mostri") {
         nomeManuale = "manuale-mostri"
+        nomejson = "Manuale_dei_Mostri"
     } else if (titolo=="Manuale del Dungeon Master") {
         nomeManuale = "manuale-dungeon-master"
+        nomejson = "Manuale_del_Dungeon_Master"
     } else if (titolo=="Calderone Omnicomprensivo di Tasha") {
         nomeManuale = "manuale-tasha"
+        nomejson = "Manuale_di_Tasha"
     } else if (titolo=="Giuda Omnicomprensiva di Xanathar") {
         nomeManuale = "manuale-xanathar"
+        nomejson = "Manuale_di_Xanathar"
     } else if (titolo=="Eberron: Rising from the Last War") {
         nomeManuale = "manuale-eberron"
+        nomejson = "Manuale_di_Eberron"
     } else {
         manualePresente = false
     }
 
     if (manualePresente) {
-        fetch(`http://localhost:5000/manuali/search/${nomeManuale}`)
+        fetch(`http://localhost:5000/json/${nomejson}.json`)
         .then(response => response.json())
-        .then(data => loadMain(data['data']));
+        .then(data => loadMain(data));
     } else {
         manualeNonTrovato();
     }
@@ -38,8 +45,9 @@ async function loadMain(data) {
     const title = document.getElementById("div-titolo")
 
     title.innerHTML = `<h1>${data.titolo}</h1>`;
+    console.log(data.nomepdf)
 
-    fetch('http://localhost:5000/html-mains/manuali.html')
+    fetch('http://localhost:5000/html/manuali.html')
     .then(response => response.text())
     .then((html) => {
         main.innerHTML = eval(`\`${html}\``)

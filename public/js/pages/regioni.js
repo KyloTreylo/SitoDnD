37fd358eaa85930c9dfc6150a8bc35d7175385a9
Ghesh05 <samuel24ibrahimi@gmail.com>
2024-03-01 11:25:46 +0100

    samuel aggiunte

--- public/js/pages/regioni.js
+++
@@ -1,7 +0,0 @@
-async function manualeNonTrovato() {
-    const main = document.querySelector("main")
-    const title = document.getElementById("div-title")
-
-    title.innerHTML = `<h1>Manuale non disponibile nell'archivio!</h1>`;
-    main.innerHTML = `<img src="../../img/manualeinesistente.png" alt="Error404">`
-}
